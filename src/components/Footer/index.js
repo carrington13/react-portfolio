2cc81386375902ef3554c84cd52c032ada576ccd
Mediogre <caseyarrington13@gmail.com>
2021-07-02 21:22:12 -0500

    Footer component w/o styling

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <div>&copy;2021 by Casey Arrington</div>
+        </footer>
+    )
+}
+
+export default Footer
