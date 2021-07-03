1586be32ca1eb4e677e7a4479ddc5115073581a4
Mediogre <caseyarrington13@gmail.com>
2021-07-02 21:19:39 -0500

    basic Header component without style

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Casey Arrington</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">
+                            Projects
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#resume">
+                            Resume
+                        </a>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+};
+
+export default Header
