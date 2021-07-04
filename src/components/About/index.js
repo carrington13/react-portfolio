6811156ffc525477fc97ff60b9d92eaba428e022
Mediogre <caseyarrington13@gmail.com>
2021-07-04 15:28:37 -0500

    form structure, need to add validation and style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//import { Box, Heading, Paragraph } from 'grommet'
 
 
 function About () {
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, FormField, TextInput, TextArea, Box, Button} from 'grommet'
 
 
 function Contact () {
+
+    const [formValue, setFormValue] = useState({})
     return (
-        <div>
-            <h2 id="contact">Contact!</h2>
-        </div>
+        <Form
+            value={formValue}
+            onChange={nextValue => setFormValue({nextValue})}
+            onReset={() => setFormValue({})}
+            // To Do something Later
+            validate="blur"
+            onSubmit={({ formValue }) => {
+                console.log(formValue);
+            }}
+        >
+            <FormField name="name" htmlFor="name-input" label="Name:">
+                <TextInput id="name-input" name="name" />
+            </FormField>
+            <FormField name="email" htmlFor="email-input" label="Email:">
+                <TextInput id="email-input" name="email" />
+            </FormField>
+            <FormField name="message" htmlFor="message-input" label="Message:" >
+                <TextArea id="message-input" name="message" />
+            </FormField>
+            <Box direction="row" gap="medium">
+                <Button type="submit" primary label="Submit" />
+                <Button type="reset" label="Reset" />
+            </Box>
+
+        </Form>
     );
 }
 
